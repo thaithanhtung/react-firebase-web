4897fb28da38fe5325b6454518758c063e64fa7d
tungthai <thajcaca@gmail.com>
2019-06-09 20:46:45 +0700

    add code

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyBVorRwdxriy1kMXAyesTbc23fDlXh9NVc",
+  authDomain: "code-backend-demo-firebase.firebaseapp.com",
+  databaseURL: "https://code-backend-demo-firebase.firebaseio.com",
+  projectId: "code-backend-demo-firebase",
+  storageBucket: "code-backend-demo-firebase.appspot.com",
+  messagingSenderId: "488671772047",
+  appId: "1:488671772047:web:dc3cc5fc971b6fef"
+};
